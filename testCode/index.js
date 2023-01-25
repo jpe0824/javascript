e8789da6c28d612dcae3587f12eeb00e556c9785
jpe0824 <jason.e24@gmail.com>
2023-01-25 16:49:57 -0700

    WIP test code program

--- testCode/index.js
+++ testCode/index.js
@@ -1,29 +1,48 @@
-console.log('loading...');
-const fs = require('fs');
+function main() {
+    console.log('loading...');
+    const fs = require('fs');
+    let warningCount = 0;
 
-const regex = /(\b|\s)var /g;
+    const catchingVar = /(\b|\s)var /;
+    const catchingMy = /\b[mM]y[A-Z]/;
 
-const args = process.argv.slice(2);
-const testFile = args.pop();
-let text = fs.readFileSync(testFile, 'utf8');
-let lines = text
-    .split(/\n|\r\n/gm)
-    .map((line, i) => `${i+1} ${line}`)
-    .filter((line) => !/^\d+\s*$/gm.test(line));
+    const args = process.argv.slice(2);
+    const testFile = args.pop();
+    let text = fs.readFileSync(testFile, 'utf8');
+    let lines = text
+        .split(/\n|\r\n/gm)
+        .map((line, i) => `${i+1}  ${line}`)
+        .filter((line) => !/^\d+\s*$/gm.test(line));
+
+    lines.forEach(line => {
+        // console.log(line,catchingVar.test(line),catchingMy.test(line));
+        if(catchingVar.test(line)) {
+            console.log(`\x1B[31mWARNING: var used on line:\t \x1b[0m${line}`);
+            warningCount++;
+        }
+        if(catchingMy.test(line)) {
+            console.log(`\x1B[31mWARNING: myName used on line:\t \x1b[0m${line}`);
+            warningCount++;
+        }
+    });
 
 
+    if (warningCount > 0) console.log(`\x1b[41mWARNING COUNT: ${warningCount}`);
+    if (warningCount == 0) console.log(`\x1b[42mWARNING COUNT: ${warningCount}`);
+    return;
+}
 
-
+main();
 
 // lines.forEach((line, i) => {
 //     if(!/^\s*$/gm.test(line)) lines2.push(i + 1 + ' ' + line);
 // });
 
-for (let i = 0; i < lines2.length; i++) {
-    // RULE no var
-    if (lines2[i].includes('\bvar ')) {
-        console.log(lines2[i]);
-        console.log('No var\n');
-    }
-    // RULE ... other rules
-}
+// for (let i = 0; i < lines2.length; i++) {
+//     // RULE no var
+//     if (lines2[i].includes('\bvar ')) {
+//         console.log(lines2[i]);
+//         console.log('No var\n');
+//     }
+//     // RULE ... other rules
+// }
--- testCode/test.js
+++ testCode/test.js
@@ -1,6 +1,14 @@
-let variety = true;
-var total = 0;
+var myVariety = 1;
+myVariety = 2;
+myVariety = 3;
+myVariety = 4;
+var mysticAnimal;
+myVariety = 5;
+
+let total = 0;
 for (var i = 1; i <= 100; i++) {
     total += 1;
 }
+let name = false;
+
 console.log(total);
