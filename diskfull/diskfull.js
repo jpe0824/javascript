91bcb17b90bf47661618c1d8444f1f5d5694029a
jpe0824 <jason.e24@gmail.com>
2023-02-06 08:39:55 -0700

    updates to command interface

--- diskfull/diskfull.js
+++ diskfull/diskfull.js
@@ -1,8 +1,16 @@
 //Positive req:
 const fs = require("fs");
 const dirArr = [];
+let commands = {
+  p: "./",
+  h: false,
+  s: false,
+  m: false,
+  t: 1,
+  b: false,
+};
 
-function postOrder(path) {
+function walkTree(path) {
   const dirEntries = fs.readdirSync(path, { withFileTypes: true });
   for (let dirEntry of dirEntries) {
     //directories
@@ -24,16 +32,18 @@ function postOrder(path) {
         if (dir.parent == tmpDir.name) tmpDir.dirChildren.push(dir);
       }
 
-      postOrder(`${path}${dir.name}`);
+      walkTree(`${path}${dir.name}`);
     } else if (dirEntry.isFile()) {
       //files
       let size = fs.statSync(`${path}${dirEntry.name}`).size;
       let parent = path.split("/");
+      let exten = dirEntry.name.split(".");
       parent.pop();
 
       let file = {
         name: `${dirEntry.name}`,
         size: size,
+        exten: `${exten[1]}`,
         parent: `${parent.pop()}/`,
       };
 
@@ -51,11 +61,28 @@ function postOrder(path) {
 }
 
 function displayDir(arr) {
+  if (commands.s) {
+    switch (commands.s) {
+      //todo - validate sorting functions actually working
+      case "alpha":
+        //sort by name
+        arr.children.sort((a, b) => a.name - b.name);
+        break;
+      case "exten":
+        //sort by extenstion
+        arr.children.sort((a, b) => a.exten - b.exten);
+        break;
+      default:
+        //sort by size;
+        arr.children.sort((a, b) => b.size - a.size);
+        break;
+    }
+  }
   //display directories
-  console.group(`${arr.name} ${arr.size} bytes`);
+  console.group(`${arr.name}   ${arr.size.toLocaleString()} bytes`);
   //display children
   arr.children.forEach((child) => {
-    console.log(`${child.name} ${child.size} bytes`);
+    console.log(`${child.name}   ${child.size.toLocaleString()} bytes`);
   });
   arr.dirChildren.forEach((child) => {
     displayDir(child);
@@ -64,17 +91,55 @@ function displayDir(arr) {
 }
 
 function main() {
-  const args = process.argv.slice(2);
-  if (args.includes("-h") || args.includes("--help")) {
+  const args = process.argv;
+
+  args.forEach((arg, index, array) => {
+    // console.log(array);
+    // console.log(index + ": " + arg);
+
+    switch (arg) {
+      case "-h":
+      case "--help":
+        commands.h = true;
+        break;
+      case "-p":
+        commands.p = "./";
+        break;
+      case "--path":
+        commands.p = array[index + 1];
+        break;
+      case "-s":
+      case "--sort":
+        switch (array[index + 1]) {
+          case "alpha":
+            commands.s = "alpha";
+            break;
+          case "exten":
+            commands.s = "exten";
+            break;
+          case "size":
+            commands.s = "size";
+            break;
+          default:
+            commands.s = false;
+            break;
+        }
+      default:
+        break;
+    }
+  });
+
+  // process.argv.forEach(function (val, index, array) {
+  //   console.log(index + ": " + val);
+  // });
+  console.log(commands);
+  if (commands.h) {
     let text = fs.readFileSync("help.txt", "utf8");
     console.log(`\n${text}\n`);
     return;
   }
-
-  command = args.pop();
-  postOrder(command);
+  walkTree(commands.p);
   displayDir(dirArr[0]);
-  
 }
 
 main();
