87a19c7631962e4fa24b1dfc264024699e0bb647
jpe0824 <jason.e24@gmail.com>
2023-03-23 21:30:22 -0600

    Style changes, changed color of file in path

--- diskfull/bigstuffV2/bigstuffv2.js
+++ diskfull/bigstuffV2/bigstuffv2.js
@@ -4,56 +4,54 @@ Jason Edman
 CS3380-001
 */
 
-const fs = require("fs")
-const filesize = require("filesize")
-const chalk = require("chalk")
-const { globIterateSync } = require("glob")
+const fs = require('fs')
+const filesize = require('filesize')
+const chalk = require('chalk')
+const { globIterateSync } = require('glob')
 
 const THRESHOLD_MULT = 1_000_000_000
 
 const fileArr = []
 const commands = {
-  path: "./**/*",
+  path: './**/*',
   help: false,
-  helpLang: "en",
+  helpLang: 'en',
   sort: false,
   metric: false,
   threshold: false,
 }
 
 let fileCount = 0
-let totalSize = 0
 
 const sortAlpha = (a, b) => (a.fileName.toLowerCase() === b.fileName.toLowerCase()) ? 0
                             : (a.fileName.toLowerCase() < b.fileName.toLowerCase() ? -1 : 1)
-const sortExten = (a, b) => (a.fileName.split(".").pop() === b.fileName.split(".").pop()) ? 0
-                            : (a.fileName.split(".").pop() < b.fileName.split(".").pop() ? -1 : 1)
+const sortExten = (a, b) => (a.fileName.split('.').pop() === b.fileName.split('.').pop()) ? 0
+                            : (a.fileName.split('.').pop() < b.fileName.split('.').pop() ? -1 : 1)
 const sortSize = (a, b) => b.size - a.size
 const sortOrder = {
-  "alpha": sortAlpha,
-  "exten": sortExten,
-  "size": sortSize
+  'alpha': sortAlpha,
+  'exten': sortExten,
+  'size': sortSize
 }
 
-function callHelp(lang="en") {
-  let helpText = fs.readFileSync("help.txt", "utf8")
-  if(lang == "es") helpText = fs.readFileSync("help_es.txt", "utf8")
+function callHelp(lang='en') {
+  let helpText = fs.readFileSync('help.txt', 'utf8')
+  if (lang == 'es') helpText = fs.readFileSync('help_es.txt', 'utf8')
   console.log(`\n${helpText}\n`)
   return
 }
 
-function globFiles(path="./**/*") {
+function globFiles(path='./**/*') {
   const tick = makeAnimation()
-  for(const file of globIterateSync(path)) {
+  for (const file of globIterateSync(path)) {
     try {
       tick()
       let size = fs.statSync(file).size
 
       fileCount += 1
-      totalSize += size
-      if(size < commands.threshold) continue
+      if (size < commands.threshold) continue
 
-      if(file.includes(".")) {
+      if (file.includes('.')) {
         fileObj = {
           fileName: file,
           size: size,
@@ -67,29 +65,27 @@ function globFiles(path="./**/*") {
 }
 
 function displayFiles() {
-  if(commands.sort) fileArr.sort(sortOrder[commands.sort])
-  let total = 0
+  if (commands.sort) fileArr.sort(sortOrder[commands.sort])
 
-  if(commands.metric) total = filesize(totalSize)
-  else total = totalSize
-
-  const longestPathLen = Math.max(...fileArr.map(item => item.fileName.length))
+  const longestPathLen = Math.max(...fileArr.map(item => item.fileName.length + 10))
   const longestSizeLen = Math.max(...fileArr.map(item => item.size.length))
+  for (const file of fileArr) {
+    const fileName = file.fileName.split('/').slice(-1)[0]
+    const coloredFileName = chalk.blueBright(fileName)
+    const coloredFileString = file.fileName.replace(fileName, coloredFileName)
 
-  for(const file of fileArr) {
-    if(commands.metric) file.size = filesize(file.size)
+    if (commands.metric) file.size = filesize(file.size)
     else file.size = file.size.toString()
 
-    console.log(`${chalk.green(file.fileName.padEnd(longestPathLen+2))}${chalk.yellow(file.size.padStart(longestSizeLen))}`)
+    console.log(`${chalk.green(coloredFileString.padEnd(longestPathLen))}${chalk.yellow(file.size.padStart(longestSizeLen))}`)
   }
-  console.log(`\nTotal size: ${chalk.yellow(total)} between ${chalk.yellow(fileCount)} files, in directory: ${commands.path}`)
 }
 
 function makeAnimation() {
-  let chars = ["⠙", "⠘", "⠰", "⠴", "⠤", "⠦", "⠆", "⠃", "⠋", "⠉"]
+  let chars = ['⠙', '⠘', '⠰', '⠴', '⠤', '⠦', '⠆', '⠃', '⠋', '⠉']
   let n = 0
   return () => {
-      if(n % 2131) process.stdout.write(`\r${chars[n % chars.length]} Loading...`)
+      if (n % 2131) process.stdout.write(`\r${chars[n % chars.length]} Loading...`)
       n++
   }
 }
@@ -99,42 +95,42 @@ function main() {
   args.forEach((arg, index, commandsArr) => {
     nextArg = commandsArr[index + 1]
     switch(arg) {
-      case "-h":
-      case "--help":
-        if(!nextArg) commands.helpLang = "en"
-        else if(nextArg.includes("-")) commands.helpLang = "en"
+      case '-h':
+      case '--help':
+        if (!nextArg) commands.helpLang = 'en'
+        else if (nextArg.includes('-')) commands.helpLang = 'en'
         else commands.helpLang = nextArg
         commands.help = true
         break
-      case "-p":
-      case "--path":
-        if(!nextArg) break
-        else if(nextArg.includes("-")) break
+      case '-p':
+      case '--path':
+        if (!nextArg) break
+        else if (nextArg.includes('-')) break
         else commands.path = nextArg
         break
-      case "-s":
-      case "--sort":
-        if(nextArg === "alpha") commands.sort = "alpha"
-        else if(nextArg === "exten") commands.sort = "exten"
-        else if(nextArg === "size") commands.sort = "size"
-        else if(nextArg.includes("-")) console.log(chalk.yellow(`\nWARNING: Not sorted. use: -s 'alpha', 'exten', 'size'.\n`))
+      case '-s':
+      case '--sort':
+        if (nextArg === 'alpha') commands.sort = 'alpha'
+        else if (nextArg === 'exten') commands.sort = 'exten'
+        else if (nextArg === 'size') commands.sort = 'size'
+        else if (nextArg.includes('-')) console.log(chalk.yellow(`\nWARNING: Not sorted. use: -s 'alpha', 'exten', 'size'.\n`))
         else console.log(chalk.yellow(`\nWARNING: Not sorted. Arg '${arg} ${nextArg}' not accepted, use: 'alpha', 'exten', 'size'.\n`))
         break
-      case "-t":
-      case "--threshold":
-        if(!nextArg) commands.threshold = 1 * THRESHOLD_MULT
-        else if(nextArg.includes("-")) commands.threshold = 1 * THRESHOLD_MULT
+      case '-t':
+      case '--threshold':
+        if (!nextArg) commands.threshold = 1 * THRESHOLD_MULT
+        else if (nextArg.includes('-')) commands.threshold = 1 * THRESHOLD_MULT
         else commands.threshold = nextArg * THRESHOLD_MULT
         break
-      case "-m":
-      case "--metric":
+      case '-m':
+      case '--metric':
         commands.metric = true
         break
       default:
         break
     }
   })
-  if(commands.help) {
+  if (commands.help) {
     callHelp(commands.helpLang)
     return
   }
@@ -142,13 +138,13 @@ function main() {
   try{
     globFiles(commands.path)
   } catch {
-    console.log(chalk.red("An error occured while trying to glob files. Please check all args"))
+    console.log(chalk.red('An error occured while trying to glob files. Please check all args'))
     callHelp(commands.helpLang)
     return
   }
 
-  if(fileCount < 1){
-    console.log(chalk.red(`Error: Could not find files, please check file path or glob pattern.  use: -p "dir/**/*"`))
+  if (fileCount < 1){
+    console.log(chalk.red(`Error: Could not find files, please check file path or glob pattern.  use: -p 'dir/**/*'`))
     return
   }
 
--- diskfull/something.js
+++ diskfull/something.js
@@ -1,113 +1,17 @@
-
-
-function startLoadingBar() {
-  let progress = 0;
-  const progressBarLength = 60;
-  const interval = setInterval(() => {
-    progress += 2;
-    const completed = Math.round(progressBarLength * (progress / 100));
-    const remaining = progressBarLength - completed;
-    const progressBar = (`[${' '.repeat(completed)}${' '.repeat(remaining)}]`);
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    process.stdout.write(chalk.green('Loading... ') + chalk.bgGreen(`${progress}% ${progressBar}`));
-  }, 100);
-
-  // Return the interval ID so we can clear it later
-  return interval;
-}
-
-function updateLoadingBar(completed, total) {
-  const progressBarLength = 60;
-  const completedLength = Math.round(progressBarLength * (completed / total));
-  const remainingLength = progressBarLength - completedLength;
-  const progressBar = '[' + chalk.bgGreen(`${' '.repeat(completedLength)}`) + chalk.bgGray(`${' '.repeat(remainingLength)}`) + ']';
-  process.stdout.clearLine();
-  process.stdout.cursorTo(0);
-  process.stdout.write(chalk.green(`Loading... ${completed}%`) + (`${progressBar}`));
-}
-
-function myFunction() {
-  console.log('Function started!');
-
-  // Start the loading bar
-  const interval = startLoadingBar();
-
-  let i = 0;
-  while (i < 1000000000) {
-    i++;
-  }
-  clearInterval(interval);
-  updateLoadingBar(33, 100);
-
-  // Start the loading bar again
-  const interval2 = startLoadingBar();
-
-  while (i < 2000000000) {
-    i++;
-  }
-  clearInterval(interval2);
-  updateLoadingBar(66, 100);
-
-  // Start the loading bar again
-  const interval3 = startLoadingBar();
-
-  while (i < 3000000000) {
-    i++;
-  }
-  clearInterval(interval3);
-  updateLoadingBar(100, 100);
-
-  // Print the completed message
-  process.stdout.clearLine();
-  process.stdout.cursorTo(0);
-  console.log('Function completed!');
-}
-
-const fs = require('fs');
-const path = require('path');
-const glob = require('glob');
 const chalk = require('chalk');
 
-function printFileSize(filepath, depth) {
-  let stats = fs.statSync(filepath);
-  let size = stats.size;
+const files = [
+  'dir1/dir2/dir3/file1.txt',
+  'dir1/dir2/dir3/file2.txt',
+  'dir1/dir2/dir3/file3.txt',
+  'file4.txt',
+  'file5.txt',
+];
 
-  if (stats.isDirectory()) {
-    console.log(chalk.blue(`${' '.repeat(depth)}${path.basename(filepath)}/`));
-    let files = glob.sync(`${filepath}/**/*`, {nodir: true});
-    let dirSize = 0;
-    for (let i = 0; i < files.length; i++) {
-      let fileStats = fs.statSync(files[i]);
-      dirSize += fileStats.size;
-    }
-    size = dirSize;
-  } else {
-    console.log(`${' '.repeat(depth)}${path.basename(filepath)}`);
-  }
+files.forEach((file) => {
+  const fileName = file.split('/').slice(-1)[0];
+  const coloredFileName = chalk.blueBright(fileName);
+  const coloredFileString = file.replace(fileName, coloredFileName);
 
-  console.log(`${' '.repeat(depth + 2)} ${chalk.green(`${size}`)}`);
-}
-
-let args = process.argv.slice(2);
-let filepath = args[0] || './';
-
-console.log(chalk.yellow(`Calculating file sizes in ${filepath}\n`));
-
-let files = glob.sync(`${filepath}/**/*`);
-let dirCount = 0;
-let fileCount = 0;
-let totalSize = 0;
-
-for (let i = 0; i < files.length; i++) {
-  let stats = fs.statSync(files[i]);
-  if (stats.isDirectory()) {
-    dirCount++;
-  } else {
-    fileCount++;
-    totalSize += stats.size;
-  }
-  printFileSize(files[i], files[i].split('/').length - 1);
-}
-console.log(chalk.yellow(`\n${fileCount} file${fileCount === 1 ? '' : 's'}, ${dirCount} director${dirCount === 1 ? 'y' : 'ies'}, ${chalk.green(`${totalSize}`)}`));
-
+  console.log(coloredFileString);
+});
