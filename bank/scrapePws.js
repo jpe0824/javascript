2c79a75533ef34c8d9e80da3758aa360f97df7d7
jpe0824 <jason.e24@gmail.com>
2023-03-30 16:19:43 -0600

    scrapePws.js finished

--- bank/scrapePws.js
+++ bank/scrapePws.js
@@ -1,91 +1,94 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+const puppeteer = require("puppeteer")
+const fs = require("fs")
 
-const OUTPUT_FILE = './mcupws.json';
-const tick = makeAnimation();
-const alphaNumRegex = /^[A-Za-z0-9]/;
+const OUTPUT_FILE = "./mcupws.json"
+const tick = makeAnimation()
+const alphaNumRegex = /^[A-Za-z0-9]/
+let pwArr = []
 
 function makeAnimation() {
-    let chars = ['⠙', '⠘', '⠰', '⠴', '⠤', '⠦', '⠆', '⠃', '⠋', '⠉'];
-    let n = 0;
-    return () => {
-        process.stdout.write(`\r${chars[n % chars.length]} Scrape in progress...`);
-        n++;
-    };
+  const numBars = 50
+  return (currPage, totalPages) => {
+    const percent = currPage / (totalPages + 1)
+    const numFilledBars = Math.floor(percent * numBars)
+    const numEmptyBars = numBars - numFilledBars
+    const filledBar = "█".repeat(numFilledBars)
+    const emptyBarLength = numEmptyBars > 0 ? numEmptyBars - 1 : 0
+    const emptyBar = " ".repeat(emptyBarLength)
+    const bar = `[${filledBar}${emptyBar}]`
+    process.stdout.write(`\r${bar} Scraping... ${Math.floor(percent * 100)}%`)
+  }
 }
 
 (async () => {
-    let pwDict = {};
-    let lastEntry = null;
+  let lastEntry = null
 
-    try {
-        const data = await fs.promises.readFile(OUTPUT_FILE, 'utf-8');
-        pwDict = JSON.parse(data);
-        const entries = Object.keys(pwDict);
-        lastEntry = entries.length > 0 ? entries[entries.length - 1] : null;
-        if (lastEntry == 10000) {
-            console.log(`${OUTPUT_FILE} already up to date`)
-            return
+  try {
+    const data = await fs.promises.readFile(OUTPUT_FILE, "utf-8")
+    pwArr = JSON.parse(data)
+    let pwArrLen = pwArr.length
+    lastEntry = pwArrLen > 0 ? pwArrLen : null
+    if (pwArrLen == 9993) {
+      console.log(`\n${OUTPUT_FILE} already up to date.`)
+      console.log(
+        `Delete ${OUTPUT_FILE} and try again if you'd like to overwrite.\n`
+      )
+      return
+    }
+    console.log(`\nResuming scraping from entry #: ${lastEntry}\n`)
+  } catch (err) {
+    process.stdout.write(`\rScraping...`)
+  }
+
+  tick(1, 100)
+
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1080, height: 1024 })
+  const baseUrl = "https://www.passwordrandom.com/most-popular-passwords"
+
+  let currPage = 1
+  if (lastEntry) {
+    currPage = Math.floor(lastEntry / 100) + 1
+    if (lastEntry % 100 === 0) currPage--
+  }
+
+  for (; currPage <= 100; currPage++) {
+    tick(currPage, 100)
+
+    await page.goto(`${baseUrl}/page/${currPage}`)
+    // Set screen size
+    const result = await page.evaluate(() => {
+      const rows = document.querySelectorAll("#cntContent_lstMain tr")
+      return Array.from(rows, (row) => {
+        const columns = row.querySelectorAll("td")
+        return Array.from(columns, (column) => column.innerText)
+      })
+    })
+
+    for (let currPw = 0; currPw <= 100; currPw++) {
+      if (result[currPw][0]) {
+        if (
+          lastEntry === null ||
+          Number(result[currPw][0]) > Number(lastEntry)
+        ) {
+          pwArr.push(result[currPw][1])
         }
-        console.log(`Resuming scraping from entry #: ${lastEntry}`);
-    } catch (err) {
-        console.log(`Writing file from start`);
+      }
     }
 
-    tick();
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    const baseUrl = 'https://www.passwordrandom.com/most-popular-passwords';
+    const filteredArr = pwArr.filter(
+      (item, index) => alphaNumRegex.test(item) && pwArr.indexOf(item) === index
+    )
+    await fs.promises.writeFile(
+      OUTPUT_FILE,
+      JSON.stringify(filteredArr, null, 2),
+      "utf-8"
+    )
+  }
 
-    let currPage = 1;
-    if (lastEntry) {
-        currPage = Math.floor(lastEntry / 100) + 1;
-        if (lastEntry % 100 === 0) currPage--
-    }
-
-    for (; currPage <= 100; currPage++) {
-        tick();
-        await page.goto(`${baseUrl}/page/${currPage}`);
-        tick();
-        // Set screen size
-        await page.setViewport({ width: 1080, height: 1024 });
-        tick();
-        const result = await page.evaluate(() => {
-            const rows = document.querySelectorAll('#cntContent_lstMain tr');
-            return Array.from(rows, (row) => {
-                const columns = row.querySelectorAll('td');
-                return Array.from(columns, (column) => column.innerText);
-            });
-        });
-
-        for (let pw of result) {
-            tick();
-            if (pw[0]) {
-                let pwData = {};
-                if (alphaNumRegex.test(pw[1])) {
-                    pwData = {
-                        password: pw[1],
-                        md5: pw[2],
-                        length: pw[3],
-                        lowerCount: pw[4],
-                        upperCount: pw[5],
-                        numCount: pw[6],
-                    };
-                }
-                if ((lastEntry === null) || (Number(pw[0]) > Number(lastEntry))) {
-                    pwDict[pw[0]] = pwData;
-                    await fs.promises.writeFile(
-                        OUTPUT_FILE,
-                        JSON.stringify(pwDict, null, 2),
-                        'utf-8'
-                    );
-                }
-            }
-        }
-    }
-
-    await browser.close();
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    console.log(`Scrape complete, data exported to file: ${OUTPUT_FILE}`);
-})();
+  await browser.close()
+  process.stdout.clearLine()
+  process.stdout.cursorTo(0)
+  console.log(`Scrape complete, ${OUTPUT_FILE} updated\n`)
+})()
